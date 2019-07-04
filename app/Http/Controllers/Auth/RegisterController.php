28a87b41e0e10f44101a7393eac0c99cd6074063
Storm <t163806@vanlanguni.vn>
2019-07-04 12:07:16 +0700

    update redirect link to user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin/home';
+    protected $redirectTo = '/admin/event';
 
     /**
      * Create a new controller instance.
